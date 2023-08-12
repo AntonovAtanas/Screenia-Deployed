a842c13a8ab25f1a915355cdea9156b374bf90b5
AntonovAtanas <atanas.antonov1@gmail.com>
2023-08-12 11:52:58 +0300

    Update endpoints.ts

--- src/app/environments/endpoints.ts
+++ src/app/environments/endpoints.ts
@@ -4,3 +4,10 @@ export const ENDPOINT = {
     reviews: 'https://screenia-server.onrender.com/movies/details',
     users: 'https://screenia-server.onrender.com/users'
 }
+
+// export const ENDPOINT = {
+//     forum: 'https://localhost:3000/forum',
+//     movies: 'https://localhost:3000/movies',
+//     reviews: 'https://localhost:3000/movies/details',
+//     users: 'https://localhost:3000/users'
+// }
